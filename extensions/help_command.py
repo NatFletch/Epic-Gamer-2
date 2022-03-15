c1e0b72b5606ea8ac6a1e48e5921af42104b3fb9
NatFletch <nathfletche@gmail.com>
2022-03-15 00:57:11 -0500

    made the help command usable but not happy with it yet, the bot is usable too now

--- extensions/help_command.py
+++ extensions/help_command.py
@@ -5,10 +5,11 @@ color = 0xff2e2e
 
 class EpicHelpSelect(discord.ui.Select):
     def __init__(self):
-        super().__init__(placeholder="Select a Command Category",options=options)
-    async def callback(self, interaction: discord.Interaction):
-        print(interaction.data)
-        await send_cog_help()
+        super().__init__(placeholder="Select a Command Category",options=[discord.SelectOption(label="Help", description="Shows the help command")])
+
+async def callback(self, interaction: discord.Interaction):
+    print(interaction.data)
+    await send_cog_help()
 
 class EpicHelpView(discord.ui.View):
     def __init__(self, *, timeout=60):
@@ -21,34 +22,33 @@ class EpicHelpCommand(commands.HelpCommand):
         super().__init__(command_attrs=dict(hidden=True))
 
     def get_command_signature(self, command):
-        return f'{self.clean_prefix}{command.qualified_name} {command.signature}'
+        return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self.context, command)
 
     def get_ending_note(self):
-        return f'Use {self.clean_prefix} {self.invoked_with} [help] for more info on a command'
+        return f'Use {self.context.clean_prefix} {self.invoked_with} [help] for more info on a command'
 
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(title=f'{self.context.bot.name} Help Menu', description="**Categories**", color=color)
-        description = self.bot.description
+        embed = discord.Embed(title=f'{self.context.bot.user.name} Help Menu', description="**Categories**", color=color)
+        description = self.context.bot.description
 
         if description:
             embed.description = description
         
         for cog, commands in mapping.items():
             filtered = await self.filter_commands(commands, sort=False)
-            commands = [f'`{self.clean_prefix}{c.name}`' for c in filtered]
+            commands = [f'`{self.context.clean_prefix}{c.name}`' for c in filtered]
             description = " ".join(commands)
-            options = []
             if filtered:
-                options.append(discord.SelectOption(label=cog.qualified_name, description=cog.description))
-            
+                embed.add_field(name=cog.qualified_name, value=cog.description)
+                EpicHelpSelect().append_option(discord.SelectOption(label=cog.qualified_name, description=cog.description))
 
             embed.set_footer(text=self.get_ending_note())
-            await self.get_destination().send(embed=embed)
+            await self.get_destination().send(embed=embed, view=EpicHelpView())
 
     async def send_cog_help(self, cog):
         embed = discord.Embed(title=f"{cog.qualified_name} Commands", color=color)
         filtered = await self.filter_commands(cog.get_commands(), sort=False)
-        commands = [f'`{self.clean_prefix}{c.name}`' for c in filtered]
+        commands = [f'`{self.context.clean_prefix}{c.name}`' for c in filtered]
         description = " ".join(commands)
         embed.description = description
         embed.set_footer(text=self.get_ending_note())
@@ -58,9 +58,9 @@ class EpicHelpCommand(commands.HelpCommand):
         text = "No Aliases"
         if len(group.aliases) > 0:
             text = " | ".join(group.aliases)
-        embed = discord.Embed(title=f"{self.clean_prefix}{group.qualified_name}", colour=0xff0000)
+        embed = discord.Embed(title=f"{self.context.clean_prefix}{group.qualified_name}", colour=0xff0000)
         if group.help:
-            embed.description = f"**Description:** {group.help}\n**Aliases:** {text}\n{group.brief}\n**Usage:**\n```{self.clean_prefix}{group.name} {group.signature}```"
+            embed.description = f"**Description:** {group.help}\n**Aliases:** {text}\n{group.brief}\n**Usage:**\n```{self.context.clean_prefix}{group.name} {group.signature}```"
 
         if isinstance(group, commands.Group):
             filtered = await self.filter_commands(group.commands, sort=True)
@@ -68,7 +68,7 @@ class EpicHelpCommand(commands.HelpCommand):
                 text = "No Aliases"
                 if len(group.aliases) > 0:
                     text = " | ".join(command.aliases)
-                embed.add_field(name=f"{self.clean_prefix}{command.qualified_name}", value=f"**Description:** {command.help}\n**Aliases:** {text}\n{command.brief}\n**Usage:**\n```{self.clean_prefix}{command.qualified_name} {command.signature}```" or '...', inline=False)
+                embed.add_field(name=f"{self.context.clean_prefix}{command.qualified_name}", value=f"**Description:** {command.help}\n**Aliases:** {text}\n{command.brief}\n**Usage:**\n```{self.clean_prefix}{command.qualified_name} {command.signature}```" or '...', inline=False)
 
         await self.get_destination().send(embed=embed)
 
@@ -78,12 +78,12 @@ class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.old_help_command = bot.help_command
-        bot.help_command = EmbedHelpCommand()
+        bot.help_command = EpicHelpCommand()
         bot.help_command.cog = self
 
     def cog_unload(self):
         self.bot.help_command = self.old_help_command
 
 
-def setup(bot):
-    bot.add_cog(Help(bot))
+async def setup(bot):
+    await bot.add_cog(Help(bot))
--- index.py
+++ index.py
@@ -2,30 +2,31 @@ import discord
 import jishaku
 import os
 import tracemalloc
+import asyncio
 
 from secret import token
 
 from discord.ext import commands
 
-extensions = [extensions.help_command]
+extensions = ["extensions.help_command"]
 
 os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
 tracemalloc.start()
 
 class EpicGamer(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="e?", help_command=None, case_insensitive=True, intents=discord.Intents.default())
-    
-        self.load_extension("jishaku")
-        for e in extensions:
-            self.load_extension(e)
+        super().__init__(command_prefix="e?", case_insensitive=True, intents=discord.Intents.all(), help_command=None)
     
     async def on_ready(self):
-        print(f'{self.user.name} ({self.user.id}) is up and running at {self.latency * 1000}ms')
+        print(f'{self.user.name} ({self.user.id}) is up and running at {round(self.latency * 1000)}ms')
         print(f'-----------------------------')
     
     async def close(self):
-        print(f'{self.bot.name} has shutdown!')
+        print(f'{self.user.name} has shutdown!')
+
+    async def setup_hook(self):
+        for e in extensions:
+            await self.load_extension(e)
 
 
 if __name__ == "__main__":
